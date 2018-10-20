2421b95194c87c9af5b60bb6f674e22720788026
Dimitris Raviolos <dictummortuum339@gmail.com>
2018-10-20 17:58:13 +0300

    first commit

---
+++ src/ephemeral/handler.clj
@@ -0,0 +1,24 @@
+(ns ephemeral.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(def store (atom {}))
+
+(defroutes app-routes
+  (POST "/:key" {:keys [body params]}
+    (swap! store assoc (-> params :key keyword) body)
+    (response @store))
+  (GET "/:key" [key]
+    (->> key keyword (get @store) response))
+  (GET "/" []
+    (response @store)))
+
+(def app
+  (routes 
+    (-> app-routes
+        (wrap-routes wrap-json-response {:keywords? true :bigdecimals? true})
+        (wrap-routes wrap-json-body))
+    (route/not-found "nil")))
